3a521bc21aa7f20875d1625604988b580fa696d4
Valeriia <valeriaya.zabolotnaya19@gmail.com>
2024-10-21 20:03:24 +0300

    Changes

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ print(n1)
 print(n2)
 print(n3)
 print(n4)
+
+
+number = 12345
+n1 = 1
+n2 = 2
+n3 = 3
+n4 = 4
+n5 = 5
+result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+print(result)
+
+
+number = 37568
+n1 = 3
+n2 = 7
+n3 = 5
+n4 = 6
+n5 = 8
+result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+print(result)
